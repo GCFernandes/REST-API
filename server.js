b80c70f7e2c989162f25ef5aeeab8bd031cc307e
GCFernandes <guilherme.correa.fernandes@usp.br>
2019-07-11 11:36:47 -0300

    Implementar rota de criação de usuario

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 //Lista inicial com os usuarios cadastrados
 const users = [
 	
@@ -11,6 +13,8 @@ const users = [
 	{id: 2, name: 'Bill'},
 
 ];
+
+let nextid = 3
 
 //Rota da pagina inicial 
 app.get('/', (req, res) => {
@@ -26,10 +30,30 @@ app.get('/api/users', (req, res) => {
 app.get('/api/users/:id', (req, res) => {
 	const user = users.find(c => c.id === parseInt(req.params.id));
 	if (!user){
+		//404 Content not found
 		res.status(404).send('User not found');
 	} else{
 		res.send(user);
 	}
+});
+
+//Rota para criar um usuario
+app.post('/api/users', (req, res) => {
+	if(!req.body.name) {
+		// 400 Bad Request
+		res.status(400).send('No name provided')
+	}
+
+	if(req.body.name.length < 3){
+		res.status(400).send("Name should be at least 3 characters long")
+	}
+	
+	const user = {
+		id: users.length,
+		name: req.body.name
+	};
+	users.push(user);
+	res.send(user);
 });
 
 //Definindo a porta e abrindo o servidor
